7518864512ac714566c65a2b17f3dc389ad1e0e4
卢婷 <18851908011@qq.com>
2021-11-16 11:16:46 +0800

    做了一个去掉CSDN广告的插件  顺便给网页打广告

---
+++ js/content-script.js
@@ -0,0 +1,23 @@
+﻿
+
+window.onload=function(){
+	let boxs=[]
+	function $(name){
+		return document.querySelector(name)
+	}
+	boxs.push($("#mys-wrapper"))
+	boxs.push($("#recommendAdBox"))
+	boxs.push($("#bottomAd"))
+	boxs.push($("#csdn-toolbar"))
+	boxs.push($("#footerRightAds"))
+	boxs.push($(".mys-adsbygoogle"))
+	boxs.push($(".toolbar-advert"))
+	console.log(boxs[0])
+	for(let item of boxs){
+		if (undefined != item){
+			item.style.display="none"
+		}
+	}
+
+}
+
